f82b342253952f89d7c0ee66099d6d71f6d6a307
Pasi Häkkinen <pasi.hakkinen@hotmail.fi>
2018-11-09 22:51:31 +0200

    Initial commit

---
+++ precache-manifest.a8bc82e7fa353be23b82fdc9571b8db6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "71e6b050fdd737d4c868",
+    "url": "/static/js/main.71e6b050.chunk.js"
+  },
+  {
+    "revision": "d70a6fc4d7ebb398e9fb",
+    "url": "/static/js/1.d70a6fc4.chunk.js"
+  },
+  {
+    "revision": "71e6b050fdd737d4c868",
+    "url": "/static/css/main.1b138806.chunk.css"
+  },
+  {
+    "revision": "14ae820b1e123413aa47259b2b93fce6",
+    "url": "/index.html"
+  }
+];
